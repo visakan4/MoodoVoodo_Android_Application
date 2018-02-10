095a02c38d890a39806d81efe0ec55f0128bdb73
Unknown <saikishore.eppalagudem@gmail.com>
2018-02-10 17:33:41 -0400

    Changes to ConfirmationActivity and  UserInputActivity

--- app/src/main/java/com/example/visak/hackothonproject/ConfirmationActivity.java
+++ app/src/main/java/com/example/visak/hackothonproject/ConfirmationActivity.java
@@ -1,13 +1,76 @@
 package com.example.visak.hackothonproject;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class ConfirmationActivity extends AppCompatActivity {
 
+    public static int userResponse;
+    public static TextView tvQuest;
+    public static Button btnPositive, btnNegative;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+        tvQuest = (TextView) findViewById(R.id.tvQuestion);
+        btnPositive = (Button) findViewById(R.id.btnPositive);
+        btnNegative = (Button) findViewById(R.id.btnNegative);
+        userResponse = UserInput.userInput;
+
+        if(userResponse == 2){
+            //Happy
+            tvQuest.setText("Here's some great music to dance to!");
+            btnNegative.setVisibility(View.GONE);
+            btnPositive.setVisibility(View.GONE);
+            //Code to play music
+            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.happy);
+            mediaPlayer.start();
+        }
+
+        if ((userResponse == 1)){
+            //Sad
+            tvQuest.setText("Would you like us to lift your spirits?");
+            btnNegative.setText("Nah! I think I'll sulk for a bit.");
+            btnPositive.setText("You can try.");
+
+        }
+
+        if ((userResponse == 0)){
+            //Angry
+            tvQuest.setText("Would you like us to lift your spirits?");
+            btnNegative.setText("Nah! I want to punch a hole in the wall!");
+            btnPositive.setText("Sure. I could use some help!");
+
+        }
+    }
+
+    public void clkPositive(View view) {
+        if (userResponse == 1)
+        {
+            //Write code to play uplifting music
+        }
+        if (userResponse == 0)
+        {
+            //Write code to play soothing music
+        }
+    }
+
+    public void clkNegative(View view) {
+        if (userResponse == 1)
+        {
+            //Write code to play sad music
+        }
+        if (userResponse == 0)
+        {
+            //Write code to play heavy, violent music
+        }
     }
 }
--- app/src/main/java/com/example/visak/hackothonproject/UserInput.java
+++ app/src/main/java/com/example/visak/hackothonproject/UserInput.java
@@ -1,13 +1,68 @@
 package com.example.visak.hackothonproject;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserInput extends AppCompatActivity {
+    public static int userInput = 0;
+    SeekBar terminalSeekBar;
+    ConstraintLayout relativeLayout;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+
+        relativeLayout = (ConstraintLayout) findViewById(R.id.relativeLayout);
+        tv = (TextView) findViewById(R.id.textView);
+        terminalSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        relativeLayout.setBackgroundResource(R.color.def);
+        terminalSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int seekBarProgress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekBarProgress = i;
+                userInput = i;
+                 Toast.makeText( getApplicationContext(), "" + i, Toast.LENGTH_SHORT).show();
+
+                 if (i == 0) {
+                     relativeLayout.setBackgroundResource(R.color.red);
+
+                 }
+                 if(i==1){
+                     relativeLayout.setBackgroundResource(R.color.blue);
+                 }
+                 if(i==2){
+                     relativeLayout.setBackgroundResource(R.color.yellow);
+                 }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+//                Toast.makeText( getApplicationContext(), "" + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+//                Toast.makeText( getApplicationContext(), "stopTracking", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+
+    public void nextActBtnClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), ConfirmationActivity.class);
+        startActivity(intent);
     }
 }
