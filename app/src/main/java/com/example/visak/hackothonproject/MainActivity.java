4becffca1904be5037af1c55dd538fcbb60feef7
visakan <visakan4@gmail.com>
2018-02-10 17:12:16 -0400

    Face Recognition API

--- app/src/main/java/com/example/visak/hackothonproject/MainActivity.java
+++ app/src/main/java/com/example/visak/hackothonproject/MainActivity.java
@@ -1,15 +1,182 @@
 package com.example.visak.hackothonproject;
 
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.client.utils.URIBuilder;
+import cz.msebera.android.httpclient.entity.ByteArrayEntity;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.HttpClients;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     Button cameraIntent;
     Button userInputIntent;
+    Uri fileUri;
+    String result;
+    String parsedResult;
+    HashMap<String,Double> emotionsResult = new HashMap<>();
+
+    public ArrayList<String> sortHashMap(){
+        ArrayList<String> sortedEmotionsList = new ArrayList<>();
+        List<Double> emotionvalues = new ArrayList<>(emotionsResult.values());
+        List<String> emotions = new ArrayList<>(emotionsResult.keySet());
+        Collections.sort(emotionvalues);
+        for (String key: emotionsResult.keySet()) {
+            for (Double value: emotionvalues){
+                if (emotionsResult.get(key).equals(value)){
+                    sortedEmotionsList.add(key);
+                }
+            }
+        }
+
+        for (String sortedEmotions:sortedEmotionsList){
+            Log.d("SortedValues",sortedEmotions);
+        }
+
+        return sortedEmotionsList;
+    }
+
+    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            return task.execute();
+        }
+    }
+
+    private void getEmotion(){
+        final AsyncTask<Void,Void,Void> newTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                HttpClient httpClient = HttpClients.createDefault();
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                try {
+                    URIBuilder builder = new URIBuilder("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+                    URI uri = builder.build();
+                    HttpPost request = new HttpPost(uri);
+                    request.setHeader("Content-Type", "application/octet-stream");
+                    request.setHeader("Ocp-Apim-Subscription-Key", "1ab4ed76e2c8422983d06c005de774be");
+//                    BitmapFactory.Options options = new BitmapFactory.Options();
+//                    options.inSampleSize = 8;
+//                    final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),options);
+                    final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath());
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+                    byte[] b = baos.toByteArray();
+                    request.setEntity(new ByteArrayEntity(b));
+                    HttpResponse response = httpClient.execute(request);
+                    HttpEntity entity = response.getEntity();
+                    result = EntityUtils.toString(entity);
+                    if (result!=null){
+                        Log.d("Parsed Result",result);
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            JSONArray jsonArray = null;
+                            try {
+                                jsonArray = new JSONArray(result);
+                                String emotions = "";
+                                for (int i = 0; i<jsonArray.length();i++){
+                                    JSONObject jsonObject = new JSONObject(jsonArray.get(i).toString());
+                                    JSONObject scores = jsonObject.getJSONObject("scores");
+                                    double max = 0;
+                                    String emotion = "";
+                                    for (int j =0; j < scores.names().length(); j++){
+                                        emotionsResult.put(scores.names().getString(j),scores.getDouble(scores.names().getString(j)));
+                                        if (scores.getDouble(scores.names().getString(j))>max){
+                                            max = scores.getDouble(scores.names().getString(j));
+                                            emotion = scores.names().getString(j);
+                                        }
+                                    }
+                                    emotions = emotion+"\n";
+                                }
+                                parsedResult = ""+emotions;
+                                if (result!=null){
+                                    Log.d("Parsed Result",parsedResult);
+                                    sortHashMap();
+                                }
+                            }catch (Exception e){
+                                parsedResult = "No emotion Detected!!";
+                            }
+                        }
+                    });
+                }catch (Exception e){
+                    return null;
+                }
+                return null;
+            }
+        };
+        runAsyncTask(newTask);
+    }
+
+    public void captureImage(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(1);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
+        startActivityForResult(cameraIntent,100);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 100){
+            if (resultCode == RESULT_OK){
+                getEmotion();
+                Toast.makeText(getApplicationContext(),"Image Captured",Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Image not Captured",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type){
+        File mediaStorage = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"image");
+        File mediafile;
+        if (!mediaStorage.exists()){
+            if (!mediaStorage.mkdirs()){
+                Log.d("image","Failed to create Directory");
+            }
+        }
+        mediafile = new File(mediaStorage.getPath() + File.separator + "EMOTION_IMG"+".jpg");
+        return mediafile;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +184,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraIntent = (Button)findViewById(R.id.cameraIntent);
         userInputIntent = (Button)findViewById(R.id.userIntent);
+
+        cameraIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                captureImage();
+            }
+        });
 
         userInputIntent.setOnClickListener(new View.OnClickListener() {
             @Override
