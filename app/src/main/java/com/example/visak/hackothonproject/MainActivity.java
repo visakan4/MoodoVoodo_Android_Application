1510d5214307d6a3247360c40909e4639829b9ce
visakan <visakan4@gmail.com>
2018-02-10 13:08:48 -0400

    Buttons created

--- app/src/main/java/com/example/visak/hackothonproject/MainActivity.java
+++ app/src/main/java/com/example/visak/hackothonproject/MainActivity.java
@@ -1,29 +1,30 @@
 package com.example.visak.hackothonproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    /**
-     *
-     *
-     * Activity which will decide how the user is feeling???
-     *
-     *
-     *
-     * 
-     *
-     */
+    Button cameraIntent;
+    Button userInputIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraIntent = (Button)findViewById(R.id.cameraIntent);
+        userInputIntent = (Button)findViewById(R.id.userIntent);
 
-
-
-
+        userInputIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,UserInput.class);
+                intent.putExtra("flowValue",-1);
+                startActivity(intent);
+            }
+        });
     }
 }
