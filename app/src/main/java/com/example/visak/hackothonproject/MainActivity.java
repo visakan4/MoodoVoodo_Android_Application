46e13b45d95e9150f6df36b059257fbb43667938
visakan <visakan4@gmail.com>
2018-02-10 12:11:53 -0400

    First Commit

---
+++ app/src/main/java/com/example/visak/hackothonproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.visak.hackothonproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     *
+     *
+     * Activity which will decide how the user is feeling???
+     *
+     *
+     *
+     * 
+     *
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/visak/hackothonproject/UserInput.java
@@ -0,0 +1,13 @@
+package com.example.visak.hackothonproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_input);
+    }
+}
